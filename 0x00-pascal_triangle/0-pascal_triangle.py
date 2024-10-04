c29ea18ace305cf4adadbef3ad3589a0dc60f4bf
chisonia <sonya12slyvia@gmail.com>
2024-10-04 05:32:01 +0100

    Paschal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+# function to create pascal's triangle
+# n is number of rows
+# check if n is <=0 and return an empty list
+# create a list to hold the rows of the triangle
+# r is index of element in each row
+# create row with lenght r+1 uisng for loop
+# m is element in each row
+# update inner row element, with sum of elements above
+# add completed row to triangle and return triangle
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = []
+
+
+    for r in range(n):
+        row = [1] * (r + 1)
+
+        for m in range(1, r):
+            row[m] = triangle[r - 1][m-1] + triangle[r - 1][m]
+        
+        triangle.append(row)
+
+    return triangle
+
