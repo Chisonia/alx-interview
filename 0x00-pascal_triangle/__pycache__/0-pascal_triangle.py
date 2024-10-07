5a7a6dcd1b9c0981ade2706412f6674623a6a38b
chisonia <sonya12slyvia@gmail.com>
2024-10-07 14:01:37 +0100

    new update

--- 0x00-pascal_triangle/__pycache__/0-pascal_triangle.py
+++ 0x00-pascal_triangle/__pycache__/0-pascal_triangle.py
@@ -15,10 +15,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1] * (r + 1) for i in range(n)]
+    triangle = []
 
-    for r in range(2, n):
+    for r in range(n):
+        row = [1] * (r + 1)
+
         for m in range(1, r):
-            triangle[r][m] = triangle[r - 1][m-1] + triangle[r - 1][m]
+            row[m] = triangle[r - 1][m-1] + triangle[r - 1][m]
 
+        triangle.append()
     return triangle
