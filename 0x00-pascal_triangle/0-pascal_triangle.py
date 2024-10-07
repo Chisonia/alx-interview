86311b89e42410693ca46519dcf09d6bff34ec9c
chisonia <sonya12slyvia@gmail.com>
2024-10-07 14:39:30 +0100

    Corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 
-# This module is a function to creates pascal's triangle
+"""This module is a function to creates pascal's triangle"""
 
-def pascal_triangle_recursive(n):
-    #This function creates a paschal triangle
+
+def pascal_triangle(n):
+    """This function prints a paschal triangle"""
     if n <= 0:
         return []
-    if n == 1:
-        return [[1]]
-    
-    # Generate the triangle recursively for n-1 rows
-    triangle = pascal_triangle_recursive(n - 1)
-    
-    # Create the new row based on the last row of the triangle
-    last_row = triangle[-1]
-    new_row = [1] + [last_row[i - 1] + last_row[i] for i in range(1, len(last_row))] + [1]
-    
-    return triangle + [new_row]
+
+    triangle = []
+    """Initialize triangle"""
+
+    for r in range(n):
+        """ Loop to generate each row of Pascal's Triangle"""
+        row = [1] * (r + 1)
+        """Each row starts and ends with 1"""
+        for m in range(1, r):
+            """Fill in the internal values for each row"""
+            row[m] = triangle[r - 1][m-1] + triangle[r - 1][m]
+
+        triangle.append(row)
+        """Add the completed row to the triangle"""
+
+    return triangle
