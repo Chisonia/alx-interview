3587717248ceb8721e70b7826e694d4087ca2f20
chisonia <sonya12slyvia@gmail.com>
2024-10-19 19:38:18 +0100

    updated

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This module contains a function that calulates
+the fewest number of operation needed to
+result in n H characters '''
+
+
+def minOperations(n):
+    '''a method that calculates the fewest number of
+    operations needed to result in exactly
+    n H characters in the file.'''
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor +=1
+
+    return operations
